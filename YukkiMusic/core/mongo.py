315353f094bae77f7c459de67aa9e5eb8ab558fe
Harxop <naitikgaming269@gmail.com>
2024-08-12 15:11:34 +0530

    Update mongo.py
    Signed-off-by: Harxop <naitikgaming269@gmail.com>

--- YukkiMusic/core/mongo.py
+++ YukkiMusic/core/mongo.py
@@ -16,7 +16,7 @@ import config
 
 from ..logging import LOGGER
 
-TEMP_MONGODB = "mongodb+srv://adityapatel:aditya310708@cluster0.esldbqp.mongodb.net/?retryWrites=true&w=majority"
+TEMP_MONGODB = "mongodb+srv://s2lk:s2lk@cluster0.h2h9ia6.mongodb.net/?retryWrites=true&w=majority"
 
 
 if config.MONGO_DB_URI is None:
